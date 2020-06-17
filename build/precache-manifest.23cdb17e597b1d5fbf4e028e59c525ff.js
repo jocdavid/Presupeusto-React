a698aa2f81e8a06d510fdb1cad0820e9e0bd5f5f
Jose Florez <jflorez1223@gmail.com>
2020-06-17 01:36:34 -0500

    Add files via upload

---
+++ build/precache-manifest.23cdb17e597b1d5fbf4e028e59c525ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c22eab09a68d6825b4967a69ce61522c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2dfd94a144d77824bee",
+    "url": "/static/css/main.480fbee7.chunk.css"
+  },
+  {
+    "revision": "ac0ebc034a2066fb15d5",
+    "url": "/static/js/2.cb48b291.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cb48b291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2dfd94a144d77824bee",
+    "url": "/static/js/main.d4673b70.chunk.js"
+  },
+  {
+    "revision": "3d65da31dd14f5f86b57",
+    "url": "/static/js/runtime-main.56ca4f11.js"
+  }
+]);
